4556453b6292b8204d6492a36b3c31d0e6686801
matteo <matteo@delgrosso.de>
2023-12-25 18:04:34 +0100

    Add multiple guesses

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-use std::process::exit;
+
 
 fn main() {
-    let number_to_guess = rand::thread_rng().gen_range(1..=10);
-    println!("Random number: {number_to_guess}");
-    println!("Guess a number 1...10?");
+    let mut number_of_guesses = String::new();
+    println!("How man numbers would you like to guess?");
+    io::stdin()
+        .read_line(&mut number_of_guesses)
+        .expect("TODO: panic message");
+
+    let number_of_guesses: u8 = number_of_guesses.trim().parse().expect("Input error");
+    let mut numbers_to_guess: Vec<u32> = Vec::new();
+    for _ in 0..number_of_guesses {
+        numbers_to_guess.push(rand::thread_rng().gen_range(1..=10));
+    }
+    println!("{numbers_to_guess:?}");
+    println!("Generated vector with {} random numbers", numbers_to_guess.len());
+    assert_eq!(numbers_to_guess.len(), number_of_guesses.into(), "Fatal Error!");
+
     let mut guess_count = 0;
-    loop {
+    println!("Guess a number 1...10?");
+    while guess_count < number_of_guesses {
         let mut guess_str = String::new();
         io::stdin()
             .read_line(&mut guess_str)
             .expect("TODO: panic message");
 
-        let guess_num: i32 = match guess_str.trim().parse() {
+        let guess_num: u32 = match guess_str.trim().parse() {
             Ok(num) => num,
             Err(e) => {
                 println!("Parse error {e}");
@@ -22,16 +35,22 @@ fn main() {
             }
         };
 
-        println!("Your guess: {guess_num}");
-        guess_count += 1;
+        //println!("Your guess: {guess_num}");
         // if expression
-        match guess_num.cmp(&number_to_guess) {
+        match guess_num.cmp(&numbers_to_guess[guess_count as usize]) {
             Ordering::Less => println!("Too low!"),
             Ordering::Greater => println!("Too high!"),
             Ordering::Equal => {
-                println!("Correct guess after {guess_count} attempts!");
-                break;
+                guess_count += 1;
+                let remaining_guesses = number_of_guesses - guess_count;
+                if remaining_guesses > 0 {
+                    println!("Correct guess! {remaining_guesses} more to guess");
+                }
             }
         };
+    }
+    println!("Thanks for playing. Here are the numbers you guessed:");
+    for item in numbers_to_guess {
+        println!("{item}")
     }
 }
